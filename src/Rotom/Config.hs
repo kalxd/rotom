ec8bc9ad1f50794693f19966017a2a9ad870fffe
葉岬 <XGLey@protonmail.com>
2019-10-19 16:35:16 +0800

    添加数据库配置。

--- src/Rotom/Config.hs
+++ src/Rotom/Config.hs
@@ -8,24 +8,40 @@ module Rotom.Config ( XGAppConfig
                     ) where
 
 import Data.Yaml
+import Data.Aeson.Types
 
 import Data.Default (Default(..))
 import System.Directory (doesFileExist)
+import qualified Database.PostgreSQL.Simple as PG
 
 -- | 全局配置
 data XGAppConfig = AppConfig { appHost :: String
                              , appPort :: Int
+                             , appDB :: PG.ConnectInfo
                              } deriving (Show)
 
 instance Default XGAppConfig where
     def = AppConfig {..}
         where appHost = "http://localhost"
               appPort = 3000
+              appDB = PG.defaultConnectInfo { PG.connectDatabase = "rotom " }
 
 instance FromJSON XGAppConfig where
     parseJSON (Object o) = AppConfig
                            <$> o .:? "host" .!= appHost def
                            <*> o .:? "port" .!= appPort def
+                           <*> o .:? "db" .!= appDB def
+    parseJSON o = typeMismatch "Object" o
+
+instance FromJSON PG.ConnectInfo where
+    parseJSON (Object o) = let config = appDB def
+                           in PG.ConnectInfo
+                              <$> o .:? "host" .!= PG.connectHost config
+                              <*> o .:? "port" .!= PG.connectPort config
+                              <*> o .:? "user" .!= PG.connectUser config
+                              <*> o .:? "password" .!= PG.connectUser config
+                              <*> o .:? "database" .!= PG.connectDatabase config
+    parseJSON o = typeMismatch "Object" o
 
 readConfig :: IO XGAppConfig
 readConfig = do
