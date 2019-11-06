105b87da108e5173763fefd8a8de2aa06ee48fca
荀徒之 <kalxd@126.com>
2019-11-06 20:13:20 +0800

    接入Auth.

--- src/Server.hs
+++ src/Server.hs
@@ -8,17 +8,17 @@ import Servant
 
 import Rotom.Type.Config (XGAppConfig(..), readConfig)
 import Rotom.Type.App (appToHandler)
+import Rotom.Type.Auth (XGContextType, authContext)
 import Rotom.Api (API, api, apiRoute)
 import Rotom.Middleware (appMiddleware)
 
 import Network.Wai.Handler.Warp (run)
 import qualified Database.PostgreSQL.Simple as PG
 
-buildRoute :: PG.Connection -> Server API
-buildRoute conn = hoistServer api (appToHandler conn) apiRoute
-
 buildApp :: PG.Connection -> Application
-buildApp = serve api . buildRoute
+buildApp conn = serveWithContext api authContext serverApi
+    where serverApi = hoistServerWithContext api (Proxy :: Proxy XGContextType) trans apiRoute
+          trans = appToHandler conn
 
 runServer :: IO ()
 runServer = do
